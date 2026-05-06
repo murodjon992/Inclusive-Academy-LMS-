680f3001dfce6ea3372882398acea689f743ff9c
Murodjon <delux8333@gmail.com>
2026-05-06 13:10:00 +0500

    fix-toolbar

--- inclusive_talim/settings.py
+++ inclusive_talim/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "crispy_forms",
     "crispy_bootstrap5",
-    'debug_toolbar',
     'inclusive_app.apps.InclusiveAppConfig',
 ]
 
@@ -30,7 +29,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
--- inclusive_talim/urls.py
+++ inclusive_talim/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -9,8 +8,3 @@ urlpatterns = [
     path('', include('inclusive_app.urls', namespace='inclusive_app')),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
